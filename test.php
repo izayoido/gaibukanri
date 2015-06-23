1ea635e8013cb69e967d1bc04a6b5786021bebd0
村上 雅樹 <izayoido@gmail.com>
2015-06-23 22:35:36 +0900

    no message

--- test.php
+++ test.php
@@ -2,6 +2,15 @@
 	require_once("config.php");
 	require_once("lib.php");
 	require_once("common.php");
+	$db_conn = db_connect($user,$passwd,$port,$dbname,$host);
 	$result = db_query($db_conn,"select id from $channel_tbl order by id");
-	echo $result;
+	echo "$result";//Resource id #6
+	echo "aaa";
+	$rows = db_num_rows($result);
+	echo $rows;//19
+	$result = db_query($db_conn,"select id from $channel_tbl where id = 19");
+	echo $result;//Resource id #7
+	echo "<pre>";
+	var_dump($result);
+	echo "</pre>";
 ?>
